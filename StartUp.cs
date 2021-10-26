f4b3a2801442fa05a5e2ba35409037ca08258709
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-26 23:21:52 +0200

    Functions

---
+++ StartUp.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(FunctionApp46.Startup))]
+
+
+namespace FunctionApp46
+{
+    class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddScoped<ICrud, Crud>();
+        }
+    }
+}
---
+++ UserContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp46
+{
+   public class UserContext : DbContext
+    {
+        public UserContext() { }
+        public UserContext(DbContextOptions<UserContext> options)
+         : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=ASSET-10422\\SQLEXPRESS;Initial Catalog=FunctionCrud06;Integrated Security=True");
+        }
+
+
+
+    }
+}
